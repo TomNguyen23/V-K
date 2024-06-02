06ee83af841b3b61917193f652fcf1e29c646a50
TomNguyen23 <102210129@sv1.dut.udn.vn>
2024-06-02 17:12:56 +0700

    modify DC-bluetooth

--- Bluetooth/DCmotor_slave/DCmotor_slave.ino
+++ Bluetooth/DCmotor_slave/DCmotor_slave.ino
@@ -18,21 +18,51 @@ void setup() {
   BTSerial.begin(9600);
 }
 
+void DCmove_forward() {
+  analogWrite(ENA, maxSpeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+}
+
+void DCmove_backward() {
+  analogWrite(ENA, maxSpeed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+}
+
+void DCspeedUp() {
+  for (int i = 10; i <= maxSpeed; i += step) {
+    analogWrite(ENA, i);
+  }
+}
+
+void DCspeedDown() {
+  for (int i = maxSpeed; i >= 10; i -= step) {
+    analogWrite(ENA, i);
+  }
+}
+
 void loop() {
   if (BTSerial.available()) {
     char data = BTSerial.read();
     Serial.println(data);
 
     if (data == '1') {
-      analogWrite(ENA, maxSpeed);
-      digitalWrite(IN1, HIGH);
-      digitalWrite(IN2, LOW);
+      DCmove_backward();
 
     }
     else if (data == '0') {
-      analogWrite(ENA, maxSpeed);
+      DCmove_forward();
+    }
+    else if (data == '2') {
+      DCspeedUp();
+    }
+    else if (data == '3') {
+      DCspeedDown();
+    }
+    else if (data == '4') {
       digitalWrite(IN1, LOW);
-      digitalWrite(IN2, HIGH);
+      digitalWrite(IN2, LOW);    
     }
   }
 
